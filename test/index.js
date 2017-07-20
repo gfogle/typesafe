af727c1e683de402e6a3dc0c95e018e451f4f9f2
George Fogle <fogle.george@gmail.com>
2017-07-19 20:41:12 -0400

    feat: setp function on prototype for safe setting

--- test/index.js
+++ test/index.js
@@ -211,7 +211,27 @@ describe('Defining a class', () => {
 
 });
 
+
 describe('Safely accessing properties', () => {
+
+  it('safely returns top-level primitive property', (done) => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        done: Boolean
+      }
+    });
+    let instance = new Todo();
+    instance.done = true
+
+    try {
+      var found = instance.getp('done');
+
+      expect(found).to.equal(true);
+      done();
+    } catch(e) {
+      done(e);
+    }
+  });
 
   it('safely returns top-level property', () => {
     const Todo = Typesafe.defineClass({
@@ -363,6 +383,193 @@ describe('Safely accessing properties', () => {
     expect(Object.keys(name).length).to.equal(2);
     expect(Object.keys(name).indexOf('first')).to.not.equal(-1);
     expect(Object.keys(name).indexOf('last')).to.not.equal(-1);
+  });
+
+});
+
+
+describe('Safely assigning properties', () => {
+
+  it('safely assigns top-level primitive property', (done) => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        done: Boolean
+      }
+    });
+    let instance = new Todo();
+
+    try {
+      instance.setp('done', true);
+
+      expect(instance.done).to.equal(true);
+      done();
+    } catch(e) {
+      done(e);
+    }
+  });
+
+  it('safely assigns nested property', (done) => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        author: {
+          properties: {
+            name: {
+              properties: {
+                first: String,
+                last: String
+              }
+            },
+            age: Number
+          }
+        }
+      }
+    });
+    let instance = new Todo();
+
+    try {
+      instance.setp('author', {
+        name: {
+          first: 'fake',
+          last: 'fake'
+        }
+      });
+
+      expect(instance.author.name.first).to.equal('fake');
+      expect(instance.author.name.last).to.equal('fake');
+
+      done();
+    } catch(e) {
+      done(e);
+    }
+  });
+
+  it('safely assigns nested property of defined parent', (done) => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        author: {
+          properties: {
+            name: {
+              properties: {
+                first: String,
+                last: String
+              }
+            },
+            age: Number
+          }
+        }
+      }
+    });
+    let instance = new Todo();
+
+    try {
+      instance.setp('author', { name: { first: '', last: '' } });
+      instance.setp('author.name.first', 'fake');
+      instance.setp('author.name.last', 'fake');
+
+      expect(instance.author.name.first).to.equal('fake');
+      expect(instance.author.name.last).to.equal('fake');
+      done();
+    } catch(e) {
+      done(e);
+    }
+  });
+
+  it('safely throws when assigning nested property of undefined parent', (done) => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        author: {
+          properties: {
+            name: {
+              properties: {
+                first: String,
+                last: String
+              }
+            },
+            age: Number
+          }
+        }
+      }
+    });
+    let instance = new Todo();
+
+    try {
+      instance.setp('author.parent', { first: 'shouldnt', last: 'work' });
+      done();
+    } catch(e) {
+      done(e);
+    }
+  });
+
+  it('safely assigns top-level array index', () => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        author: {
+          properties: {
+            name: {
+              properties: {
+                first: String,
+                last: String
+              }
+            },
+            age: Number
+          }
+        }
+      }
+    });
+    const TodoList = Typesafe.defineClass({
+      properties: {
+        list: Array
+      }
+    });
+    var instance = new Todo();
+    let list = new TodoList();
+
+    list.setp('list', []);
+    list.setp('list[0]', instance);
+
+    const first = list.list[0];
+
+    expect(first).to.not.equal(null);
+    expect(Object.keys(first.author).length).to.equal(2);
+    expect(Object.keys(first.author).indexOf('name')).to.not.equal(-1);
+
+    expect(Object.keys(first.author.name).length).to.equal(2);
+    expect(Object.keys(first.author.name).indexOf('first')).to.not.equal(-1);
+    expect(Object.keys(first.author.name).indexOf('last')).to.not.equal(-1);
+  });
+
+  it('safely assigns top-level array index with nested properties', () => {
+    const Todo = Typesafe.defineClass({
+      properties: {
+        author: {
+          properties: {
+            name: {
+              properties: {
+                first: String,
+                last: String
+              }
+            },
+            age: Number
+          }
+        }
+      }
+    });
+    const TodoList = Typesafe.defineClass({
+      properties: {
+        list: Array
+      }
+    });
+    var instance = new Todo();
+    let list = new TodoList();
+
+    list.list = [instance];
+
+    list.setp('list', [instance]);
+    list.setp('list[0].author.name.first', 'fake');
+    list.setp('list[0].author.name.last', 'fake');
+
+    expect(list.list[0].author.name.first).to.equal('fake');
+    expect(list.list[0].author.name.last).to.equal('fake');
   });
 
 });

c71a63fd723c010ab48a97592ba198098b17ad2c
George Fogle <fogle.george@gmail.com>
2017-07-24 20:22:43 -0400

    code cleanup (#23)

--- src/index.js
+++ src/index.js
@@ -3,44 +3,12 @@ module.exports = {
   defineClass: function defineClass(defn) {
     return function classDefn() {
       var instance = {};
-      var properties = Object.keys(defn.properties || []);
-      var propName;
-      var propConfig;
-      var functions = Object.keys(defn.functions || []);
-      var fnName;
-      var fnDefn;
 
-      _createProtoFns(instance);
+      Object.getPrototypeOf(instance).getp = _getp;
+      Object.getPrototypeOf(instance).setp = _setp;
+      Object.getPrototypeOf(instance).execf = _execf;
 
-      for(var i = 0; i < properties.length; i++) {
-        propName = properties[i];
-        propConfig = defn.properties[propName];
-
-        if (propName === '__proto__') {
-          continue;
-        }
-
-        if (typeof defn.properties[propName] !== 'object') {
-          _defineProp(instance, propName, propConfig);
-        }
-        if (typeof defn.properties[propName] === 'object') {
-          _defineObjectProp(instance, propName, propConfig);
-        }
-      }
-
-      for(var j = 0; j < functions.length; j++) {
-        fnName = functions[j];
-        fnDefn = defn.functions[fnName];
-
-        if (typeof fnDefn !== 'function') {
-          throw new Error('function definitions must be functions');
-          break;
-        }
-
-        _defineFunction(instance, fnName, fnDefn);
-      }
-
-      return Object.preventExtensions(instance);
+      return _defineObjectProp(instance, null, defn);
     };
 
     /**
@@ -49,83 +17,86 @@ module.exports = {
      * 
      */
 
-    function _createProtoFns(obj) {
-      var proto = Object.getPrototypeOf(obj);
+    function _isArraySyntax(arg) {
+      return arg.indexOf('[') !== -1 && arg.indexOf(']') !== -1;
+    }
 
-      proto.getp = function(objPath) {
-        var paths = objPath.split('.');
-        var obj = this;
-        var current = obj;
+    function _getp(objPath) {
+      var paths = objPath.split('.');
+      var obj = this;
+      var current = obj;
 
-        for(var i = 0; i < paths.length; i++) {
-          if (paths[i].indexOf('[') !== -1 && paths[i].indexOf(']') !== -1) {
-            var prop = paths[i].split('[')[0];
-            var idx = Number(paths[i].split('[')[1].split(']')[0]);
+      for(var i = 0; i < paths.length; i++) {
+        if (_isArraySyntax(paths[i])) {
+          var prop = paths[i].split('[')[0];
+          var idx = Number(paths[i].split('[')[1].split(']')[0]);
 
-            current = current[prop][idx];
-          } else if (current[paths[i]]) {
-            current = current[paths[i]];
-          } else {
-            current = null;
-            break;
-          }
-        }
-
-        return current ? current : null;
-      }
-
-      proto.setp = function(objPath, value) {
-        var paths = objPath.split('.');
-        var obj = this;
-
-        for(var i = 0; i < paths.length; i++) {
-          if (paths[i].indexOf('[') !== -1 && paths[i].indexOf(']') !== -1 && i + 1 !== paths.length) {
-            var prop = paths[i].split('[')[0];
-            var idx = Number(paths[i].split('[')[1].split(']')[0]);
-
-            obj = obj[prop][idx];
-          } else if (paths[i].indexOf('[') !== -1 && paths[i].indexOf(']') !== -1 && i + 1 === paths.length) {
-            var prop = paths[i].split('[')[0];
-            var idx = Number(paths[i].split('[')[1].split(']')[0]);
-
-            if (typeof value !== 'object' || (typeof value === 'object' && Array.isArray(obj[prop]))) {
-              obj[prop][idx] = value;
-            } else {
-              _copyDefined(obj[prop][idx], value);
-            }
-            break;
-          } else if (obj.hasOwnProperty(paths[i]) && i + 1 == paths.length) {
-            if (typeof value !== 'object' || (typeof value === 'object' && Array.isArray(value))) {
-              obj[paths[i]] = value;
-            } else {
-              _copyDefined(obj[paths[i]], value);
-            }
-            break;
-          } else if (obj.hasOwnProperty(paths[i]) && obj[paths[i]]) {
-            obj = obj[paths[i]];
-          } else {
-            break;
-          }
+          current = current[prop][idx];
+        } else if (current[paths[i]]) {
+          current = current[paths[i]];
+        } else {
+          current = null;
+          break;
         }
       }
 
-      proto.execf = function(fnPath) {
-        var paths = fnPath.split('.');
-        var obj = this;
+      return current ? current : null;
+    }
 
-        for(var i = 0; i < paths.length; i++) {
-          if (paths[i].indexOf('[') !== -1 && paths[i].indexOf(']') !== -1 && i + 1 !== paths.length) {
-            var prop = paths[i].split('[')[0];
-            var idx = Number(paths[i].split('[')[1].split(']')[0]);
+    function _setp(objPath, value) {
+      var paths = objPath.split('.');
+      var obj = this;
 
-            obj = obj[prop][idx];
-          } else if (obj[paths[i]] && i + 1 == paths.length && typeof obj[paths[i]] === 'function') {
-            return obj[paths[i]].apply(obj, arguments);
-          } else if (obj.hasOwnProperty(paths[i]) && obj[paths[i]]) {
-            obj = obj[paths[i]];
+      for(var i = 0; i < paths.length; i++) {
+        var prop;
+        var idx;
+
+        if (_isArraySyntax(paths[i]) && i + 1 !== paths.length) {
+          prop = paths[i].split('[')[0];
+          idx = Number(paths[i].split('[')[1].split(']')[0]);
+
+          obj = obj[prop][idx];
+        } else if (_isArraySyntax(paths[i]) && i + 1 === paths.length) {
+          prop = paths[i].split('[')[0];
+          idx = Number(paths[i].split('[')[1].split(']')[0]);
+
+          if (typeof value !== 'object' || (typeof value === 'object' && Array.isArray(obj[prop]))) {
+            obj[prop][idx] = value;
           } else {
-            break;
+            _copyDefined(obj[prop][idx], value);
           }
+          break;
+        } else if (obj.hasOwnProperty(paths[i]) && i + 1 === paths.length) {
+          if (typeof value !== 'object' || (typeof value === 'object' && Array.isArray(value))) {
+            obj[paths[i]] = value;
+          } else {
+            _copyDefined(obj[paths[i]], value);
+          }
+          break;
+        } else if (obj.hasOwnProperty(paths[i]) && obj[paths[i]]) {
+          obj = obj[paths[i]];
+        } else {
+          break;
+        }
+      }
+    }
+
+    function _execf(fnPath) {
+      var paths = fnPath.split('.');
+      var obj = this;
+
+      for(var i = 0; i < paths.length; i++) {
+        if (_isArraySyntax(paths[i]) && i + 1 !== paths.length) {
+          var prop = paths[i].split('[')[0];
+          var idx = Number(paths[i].split('[')[1].split(']')[0]);
+
+          obj = obj[prop][idx];
+        } else if (obj[paths[i]] && i + 1 === paths.length && typeof obj[paths[i]] === 'function') {
+          return obj[paths[i]].apply(obj, arguments);
+        } else if (obj.hasOwnProperty(paths[i]) && obj[paths[i]]) {
+          obj = obj[paths[i]];
+        } else {
+          break;
         }
       }
     }
@@ -145,20 +116,13 @@ module.exports = {
     function _determineType(prop) {
       if (prop === String) {
         return 'string';
-      }
-      if (prop === Number) {
+      } else if (prop === Number) {
         return 'number';
-      }
-      if (prop === Boolean) {
+      } else if (prop === Boolean) {
         return 'boolean';
-      }
-      if (prop === Array) {
+      } else if (prop === Array) {
         return 'array';
       }
-    }
-
-    function _defineFunction(instance, name, fn) {
-      (Object.getPrototypeOf(instance))[name] = fn;
     }
 
     function _defineProp(instance, name, config) {
@@ -190,43 +154,30 @@ module.exports = {
     function _defineObjectProp(instance, name, config) {
       var obj = {};
       var properties = Object.keys(config.properties || []);
-      var propName;
-      var propConfig;
       var functions = Object.keys(config.functions || []);
-      var fnName;
-      var fnDefn;
 
-      for(var i = 0; i < properties.length; i++) {
-        propName = properties[i];
-        propConfig = config.properties[propName];
-
-        if (propName === '__proto__') {
-          continue;
+      properties.forEach(function(prop) {
+        if (prop !== '__proto__' && typeof config.properties[prop] !== 'object') {
+          _defineProp(obj, prop, config.properties[prop]);
+        } else if (prop !== '__proto__' && typeof config.properties[prop] === 'object') {
+          _defineObjectProp(obj, prop, config.properties[prop]);
         }
-        
-        if (typeof config.properties[propName] !== 'object') {
-          _defineProp(obj, propName, propConfig);
-        }
-        if (typeof config.properties[propName] === 'object') {
-          _defineObjectProp(obj, propName, propConfig);
-        }
-      }
+      })
 
-      for(var j = 0; j < functions.length; j++) {
-        fnName = functions[j];
-        fnDefn = config.functions[fnName];
-
-        if (typeof fnDefn !== 'function') {
-          throw new Error('function definitions must be functions');
-          break;
+      functions.forEach(function(fn) {
+        if (typeof config.functions[fn] !== 'function') {
+          throw new Error('function definitions must be a function');
         }
-
-        _defineFunction(obj, fnName, fnDefn);
-      }
+        (Object.getPrototypeOf(obj))[fn] = config.functions[fn];
+      });
 
       Object.preventExtensions(obj);
 
-      instance[name] = obj;
+      if (!name) {
+        return obj;
+      } else {
+        instance[name] = obj;
+      }
     }
   }
 }
